b1f25ccda6c5f4d7e7c0e48d7566081213ed7ebd
maxsch38 <schroder.maxime@icloud.com>
2024-01-03 18:31:30 +0100

    MAJ fichier boostrap

--- bootstrap-emr.sh
+++ bootstrap-emr.sh
@@ -8,3 +8,5 @@ sudo python3 -m pip install pyarrow
 sudo python3 -m pip install boto3
 sudo python3 -m pip install s3fs
 sudo python3 -m pip install fsspec
+sudo python3 -m pip install pyspark
+sudo python3 -m pip install tensorflow
